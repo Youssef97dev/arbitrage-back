56b585cbbb76c08bd1012aa0c4a3d67b73bfdc44
Youssef97dev <youssefmoussafi97@gmail.com>
2025-10-14 23:42:56 +0100

    add ssl

--- config/database.js
+++ config/database.js
@@ -1,23 +1,24 @@
-const { Pool } = require('pg');
+const { Pool } = require("pg");
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL, // <-- use this
-  ssl: process.env.NODE_ENV === 'production' 
-       ? { rejectUnauthorized: false } 
-       : false,
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { require: true, rejectUnauthorized: false }
+      : false,
 });
 
 // Test de connexion
 pool.connect((err, client, release) => {
   if (err) {
-    console.error('Erreur de connexion à la base de données:', err.stack);
+    console.error("Erreur de connexion à la base de données:", err.stack);
   } else {
-    console.log('✅ Connexion à la base de données réussie');
+    console.log("✅ Connexion à la base de données réussie");
     release();
   }
 });
 
 module.exports = {
   query: (text, params) => pool.query(text, params),
-  pool
+  pool,
 };
